5a16ea4ea0ee33b1d45b28d40f2a7ba4640cd89d
bebiegrace <bebiesarabalbuena21@gmail.com>
2022-10-14 18:23:04 +0800

    update

---
+++ 2ndactivity_update/ggfilfder/bebieNewException.java
@@ -0,0 +1,17 @@
+package ggfilfder;
+
+public class bebieNewException extends Exception {
+    public void myError(){
+    
+        System.out.println("Invalid Input! Number object must not greater that two or lower than zero!!");
+}
+public void myError1(){
+    
+    System.out.println("Invalid Input!");
+}
+
+
+
+}
+
+
