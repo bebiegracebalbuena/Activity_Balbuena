77b4f6af69ce597cfe34bc46a33d2cb1656504d5
bebiegracebalbuena <112835158+bebiegracebalbuena@users.noreply.github.com>
2022-10-15 08:20:11 +0800

    Update newException.java

--- Bebie_activity_05/newException.java
+++ Bebie_activity_05/newException.java
@@ -6,20 +6,4 @@ public class newException extends Exception {
         super(message);
     }
 
-
-
-    // public void ageError() {
-
-    //     // System.out.println("There is no name with that index " + mychoice + " Invalid
-    //     // input, please try to input another index number!");
-    //     System.out.println("Invalid Input for age!");
-    // }
-
-    // public void nameError() {
-
-    //     // System.out.println("There is no name with that index " + mychoice + " Invalid
-    //     // input, please try to input another index number!");
-    //     System.out.println("Invalid Input for Name!");
-    // }
-
 }
